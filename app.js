0dc83ff31fd02e250e001d93ae270b372093b2ac
Willian Ramon <ramonbarros.90@gmail.com>
2024-04-14 17:34:43 -0300

    Revert "Facilitando o jogo"
    This reverts commit d69e76bf6f679690e03b3108d7d6ec7e65d44f67.

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate : 1.0});
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate : 1.2});
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto.');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 5: ');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10: ');
 }
 exibirMensagemInicial();
 
